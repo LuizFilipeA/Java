c2920c742d0687dac1436998f26c16bddf885eb9
Luiz Filipe Santos <150827818+devLoue@users.noreply.github.com>
2024-11-06 14:09:35 -0300

    - última atualização: conceito de final

---
+++ JAVA Exercicios/src/Final/Filho.java
@@ -0,0 +1,15 @@
+package Final;
+
+public class Filho extends Pai{
+    /*um atributo final é semelhante á uma constante, não poderá ser alterada nem mesmo com herança e polimorfismo.
+    esta classe filho, está recebendo uma herança da classe pai pelo "extends" e mesmo assim, após este comentário,
+    iremos tentar herdar o metodo mensagem, e ele não será "reconhecido" pelo java, pois o metodo Mensagem() da classe
+    pai, recebeu uma atribuição final, porém, o metodo mensagem2() será herdado normalmente, pois não recebeu atribuição final*/
+
+    @Override
+    public void Mensagem2() { //metodo da classe pai sem final
+        super.Mensagem2();
+    }
+
+    mensagem(){} //metodo da classe pai com final (irreconhecível)
+}
---
+++ JAVA Exercicios/src/Final/Pai.java
@@ -0,0 +1,13 @@
+package Final;
+
+public class Pai {
+    public final int x = 10; //constante
+
+    public final void Mensagem(){
+        System.out.println("Teste");
+    }
+
+    public void Mensagem2(){
+        System.out.println("Teste");
+    }
+}
