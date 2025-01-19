58084f485922b63c26bc7417fb92abfbe67ab1a2
Luiz Filipe Santos <150827818+devLoue@users.noreply.github.com>
2025-01-18 21:19:34 -0300

    Vetores

---
+++ JAVA Exercicios/src/Vetores/Main.java
@@ -0,0 +1,13 @@
+package Vetores;
+//Em Java, vetores são estruturas de dados que armazenam um conjunto de valores do mesmo tipo. Também são conhecidos como arrays.
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Vetor quando você já conhece o tamanho final e os valores dele
+        int idades[] = {10, 15, 20, 25, 30};
+
+        //Vetor quando você só conhece o tamanho, mas ainda não sabe quais valores vão preencher o vetor.
+        int[] idades2 = new int[10];
+    }
+}
