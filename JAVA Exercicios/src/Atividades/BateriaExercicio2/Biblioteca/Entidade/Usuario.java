05cd74d95f9049666acdcf98af6d5f17195c9404
Luiz Filipe Santos <150827818+devLoue@users.noreply.github.com>
2025-08-18 19:55:53 -0300

    Entidade usuário

--- JAVA Exercicios/src/Atividades/BateriaExercicio2/Biblioteca/Entidade/Usuario.java
+++ JAVA Exercicios/src/Atividades/BateriaExercicio2/Biblioteca/Entidade/Usuario.java
@@ -1,4 +1,27 @@
 package Atividades.BateriaExercicio2.Biblioteca.Entidade;
 
 public class Usuario {
+    String nome;
+    Livro livroemprestado = null;
+
+    public Usuario(String nome){
+        this.nome = nome;
+    }
+
+    public void alugarLivro(Livro livroemprestado){
+        if (livroemprestado==null){
+            System.out.println("Você alugou o livro "+livroemprestado.getTitulo());
+            livroemprestado.setDisponivel(false);
+        }
+        else {
+            System.out.println("Devolva um livro para pegar outro emprestado");
+        }
+    }
+
+    public void devolverLivro(Livro livroemprestado){
+        if (livroemprestado != null){
+            System.out.println("Você devolveu o livro "+livroemprestado.getTitulo());
+            livroemprestado.setDisponivel(true);
+        }
+    }
 }
