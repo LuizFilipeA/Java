4e9824763e7c29525db25ad196283b265cdb89f4
Luiz Filipe Santos <150827818+devLoue@users.noreply.github.com>
2025-08-18 19:48:07 -0300

    Biblioteca

---
+++ JAVA Exercicios/src/Atividades/BateriaExercicio2/Biblioteca/Entidade/Autor.java
@@ -0,0 +1,12 @@
+package Atividades.BateriaExercicio2.Biblioteca.Entidade;
+
+public class Autor {
+    String nome;
+    Livro livro;
+
+    public Autor(Livro livro, String nome) {
+        this.livro = livro;
+        this.nome = nome;
+    }
+
+}
