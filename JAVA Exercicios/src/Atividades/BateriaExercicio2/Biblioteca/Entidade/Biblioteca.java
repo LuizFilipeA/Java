b810cb2c1f590daa1dc9150db7742d88d271c59f
Luiz Filipe Santos <150827818+devLoue@users.noreply.github.com>
2025-08-21 20:05:16 -0300

    Entidade Biblioteca

--- JAVA Exercicios/src/Atividades/BateriaExercicio2/Biblioteca/Entidade/Biblioteca.java
+++ JAVA Exercicios/src/Atividades/BateriaExercicio2/Biblioteca/Entidade/Biblioteca.java
@@ -1,4 +1,49 @@
 package Atividades.BateriaExercicio2.Biblioteca.Entidade;
 
+import POO.Biblioteca.Livro;
+import POO.Biblioteca.Usuario;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+Scanner
+
 public class Biblioteca {
+
+    private ArrayList<Livro> livros;
+    private ArrayList<Usuario> usuarios;
+
+    public Biblioteca(){
+        livros = new ArrayList<Livro>();
+        usuarios = new ArrayList<Usuario>();
+    }
+
+    public ArrayList<Livro> getLivros() {
+        return livros;
+    }
+
+    public void setLivros(ArrayList<Livro> livros) {
+        this.livros = livros;
+    }
+
+    public ArrayList<Usuario> getUsuarios() {
+        return usuarios;
+    }
+
+    public void setUsuarios(ArrayList<Usuario> usuarios) {
+        this.usuarios = usuarios;
+    }
+
+    public void adicionarLivro(Livro livro){
+        livros.add(livro);
+    }
+
+    public void buscarLivro(String nome){
+        for ()
+    }
+
+    public void buscarUsuario(Usuario usuario){
+        if (usuario.getNomeUsuario().equals())
+    }
+
 }
