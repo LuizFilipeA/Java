1ace1f09a4f8c0ae57238a2bc4a74fba3853cc25
LuizFilipeA <luizfilipealvessantos@outlook.com>
2025-11-10 19:46:29 -0300

    Apenas um adendo ao conteúdo de Casting (fundição)

--- JAVA Exercicios/src/Fundamentos/Casting/Main.java
+++ JAVA Exercicios/src/Fundamentos/Casting/Main.java
@@ -5,13 +5,13 @@ public class Main {
         // Exemplo De int para double
         int valorInteiro = 3;
         double valorDouble = valorInteiro;
-        System.out.println("valorInteiro: "+valorInteiro+" valorDouble: "+valorDouble);
+        System.out.println("valorInteiro: "+valorInteiro+" valorDouble: "+valorDouble); //(int = 3, double = 3.00)
 
         //Exemplo De double para int
-        double n3 = 3.3;
+        float n3 = 3.30f;
         int n4;
-        n4 = (int)n3;
-        System.out.println("n3: "+n3+" n4: "+n4);
+        n4 = (int)n3; //Um casting explícito, o java entende que nós aceitamos perder os valores após a virgula
+        System.out.println("n3: "+n3+" n4: "+n4); //(float = 3.30, int =3)
 
         //Exemplo De char para string
         char letra= 's';
@@ -19,11 +19,12 @@ public class Main {
         System.out.println("String: "+palavra);
 
         //Exemplo De string para char
-        String s = "String";
-        char c = s.charAt(5);
+        String s = "String"; //Uma string nada mais é do que um vetor de caracteres
+        char c = s.charAt(5); //Com a função charAt, somos obrigados a informar o indice, ou seja, qual char da string nós queremos armazenar, neste caso, armazenamos o char de posição 5 da palavra String, ou seja, o char "g"
         System.out.println("Char: "+c);
 
         //Exemplo De int para string
+
         String teste = String.valueOf(n4);
         System.out.println("Sim, isso é uma string"+teste);
     }
