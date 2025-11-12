3879db02d839c87dab4b675c73fb730d34d54fad
LuizFilipeA <luizfilipealvessantos@outlook.com>
2025-11-12 20:43:35 -0300

    "Explicando a função recursiva"

--- JAVA Exercicios/src/Fundamentos/Funcoes/Recursiva1.java
+++ JAVA Exercicios/src/Fundamentos/Funcoes/Recursiva1.java
@@ -2,21 +2,21 @@ package Fundamentos.Funcoes;
 
 public class Recursiva1 {
 
+    public static void a1(){
+        System.out.println("a1()" );
+        // bloco de código aqui
+    }
+
+
     public static void a2(){
-        System.out.println("Chamando função a1");
+        System.out.println("a2()");
         a1();
         //bloco de código aqui
     }
 
 
-    public static void a1(){
-        System.out.println("Chama função n" );
-        // bloco de código aqui
-    }
-
-
     public static void main (String[] args){
-        System.out.println("public static void main");
+        System.out.println("public static void main"); //Na execução do programa é possível ver que a função main (função principal) é a primeira a ser executada no nosso programa
         a2();
     }
 }
