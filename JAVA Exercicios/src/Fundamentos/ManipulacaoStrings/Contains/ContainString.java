63751a1fb32bc5ba11ad37a49823b1b5dcb7aa43
LuizFilipeA <luizfilipealvessantos@outlook.com>
2025-11-17 19:51:30 -0300

    Manipulacao de String: Contain + UpperCase

--- JAVA Exercicios/src/Fundamentos/ManipulacaoStrings/Contains/ContainString.java
+++ JAVA Exercicios/src/Fundamentos/ManipulacaoStrings/Contains/ContainString.java
@@ -4,8 +4,13 @@ public class ContainString {
 
     public static void main(String[] args) {
         String frase = "Formula mágica da paz";
+        //Verificando se a String frase contém (contain) a palavra "paz"
         boolean contemPaz = frase.contains("paz");
         System.out.println(contemPaz);
 
+        //CONTAIN + UPPER
+        String upper = frase.toUpperCase();
+        contemPaz = upper.contains("PAZ");
+        System.out.println(contemPaz);
     }
 }
