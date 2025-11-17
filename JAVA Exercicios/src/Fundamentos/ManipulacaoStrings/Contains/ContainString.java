3d46b57b47b916f28b4f5b33002c0fef7d14f949
LuizFilipeA <luizfilipealvessantos@outlook.com>
2025-11-17 19:28:45 -0300

    Manipulacao de String: Contain + String

---
+++ JAVA Exercicios/src/Fundamentos/ManipulacaoStrings/Contains/ContainString.java
@@ -0,0 +1,11 @@
+package Fundamentos.ManipulacaoStrings.Contains;
+
+public class ContainString {
+
+    public static void main(String[] args) {
+        String frase = "Formula mágica da paz";
+        boolean contemPaz = frase.contains("paz");
+        System.out.println(contemPaz);
+
+    }
+}
