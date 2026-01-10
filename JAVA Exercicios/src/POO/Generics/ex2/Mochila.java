1ca5f88f19d95c4a3ccc13452b5bf454d2204a01
LuizFilipeA <luizfilipealvessantos@outlook.com>
2026-01-10 10:51:50 -0300

    Generics Ex2 - Classe Mochila(Lista de itens)

--- JAVA Exercicios/src/POO/Generics/ex2/Mochila.java
+++ JAVA Exercicios/src/POO/Generics/ex2/Mochila.java
@@ -3,10 +3,24 @@ package POO.Generics.ex2;
 import java.util.List;
 
 public class Mochila <T>{
-
-    private List<T> mochila;
+    private List<T> mochilaTrabalho;
 
     public void adicionarItem(T item){
-        mochila.add(item);
+        try{
+         mochilaTrabalho.add(item);
+        }catch (NullPointerException npe){
+            System.out.println("A mochila está vazia (null). Inicialize a lista antes de adicionar itens.");
+        }
+
+    }
+
+    public void mostrarItens(){
+        try{
+            for (T item : mochilaTrabalho){
+                System.out.println(item.toString());
+            }
+        }catch (NullPointerException npe){
+            System.out.println("Mochila de trabalho vazia. Coloque itens na mochila antes de mostrar os itens.");
+        }
     }
 }
