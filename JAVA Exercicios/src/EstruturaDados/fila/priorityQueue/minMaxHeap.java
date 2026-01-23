bef0a3ca0186a8c2a434a865ea26ffb4ae14fb2a
LuizFilipeA <luizfilipealvessantos@outlook.com>
2026-01-23 20:35:40 -0300

    Estrutura de dados - minHeap e maxHeap

---
+++ JAVA Exercicios/src/EstruturaDados/fila/priorityQueue/minMaxHeap.java
@@ -0,0 +1,28 @@
+package EstruturaDados.fila.priorityQueue;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class minMaxHeap {
+
+    public static void main(String[] args) {
+
+        //Por padrão, o PriorityQueue funciona como um minHeap, onde o menor elemento tem a maior prioridade
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        minHeap.add(20);
+        minHeap.add(10);
+        minHeap.add(30);
+        minHeap.add(40);
+        System.out.println(minHeap.poll()); // 10
+
+        //Diferente do minHeap, o maxHeap recebe Collections.reverseOrder() como parâmetro para inverter a ordem dos elementos
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        maxHeap.add(-20);
+        maxHeap.add(-10);
+        maxHeap.add(1);
+        maxHeap.add(-30);
+        maxHeap.add(-40);
+
+        System.out.println(maxHeap.poll()); // 1
+    }
+}
