74407123743089c719b023ed3ca11840a81c4e78
LuizFilipeA <luizfilipealvessantos@outlook.com>
2026-01-27 13:24:14 -0300

    Estrutura de Dados - HashSet - CRUD Gerenciador de Nomes(únicos - Lista de festa): - CRIAR - REMOVER - CONSULTA INDIVIDUAL - EXISTE NOME? - LISTAR TODOS

---
+++ JAVA Exercicios/src/estruturaDados/hashSet/GerenciadorDeNomes.java
@@ -0,0 +1,49 @@
+package estruturaDados.hashSet;
+
+import java.util.HashSet;
+
+public class GerenciadorDeNomes {
+
+    //Atributo = a própria "lista "
+    HashSet<String> hashNomes;
+
+    //Construtor
+    public GerenciadorDeNomes(String nome){
+        this.hashNomes = new HashSet<String>();
+    }
+
+    //ADICIONAR NOME
+    public String adicionarNome(String nome){
+        if (this.hashNomes.add(nome)) { //O metodo add retorna true se o elemento foi adicionado com sucesso e false caso não seja
+            return "Nome adicionado com sucesso!";
+        } else {
+            return "Nome já existe na coleção.";
+        }
+    }
+
+    //EXISTE NOME? - CONSULTA INDIVIDUAL
+    public boolean existeNome(final String nome){
+        return this.hashNomes.contains(nome); //O metodo contains retorna true se o elemento existe na coleção e false caso não exista
+    }
+
+    //REMOVE
+    public String removerNome(final String nome){
+        if (this.hashNomes.remove(nome)){ //O metodo remove retorna true se o elemento foi removido com sucesso e false caso não seja
+            return "Nome removido com sucesso!";
+        } else {
+            return "Nome não encontrado.";
+        }
+    }
+
+    //EXIBIR TODOS OS NOMES - CONSULTA TOTAL
+    public void exibirTodosNomes(){
+        if (this.hashNomes.isEmpty()){
+            System.out.println("Lista vazia. Insira ao menos um nome");
+        }else{
+            for (String s : hashNomes){
+                System.out.println("nome: "+s);
+            }
+        }
+
+    }
+}
