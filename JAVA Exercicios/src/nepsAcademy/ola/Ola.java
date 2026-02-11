38719d99279b189d26617823c44f128c30e6b4e4
LuizFilipeA <luizfilipealvessantos@outlook.com>
2026-02-11 19:34:37 -0300

    Neps Academy Exercício 1

---
+++ JAVA Exercicios/src/nepsAcademy/ola/Ola.java
@@ -0,0 +1,24 @@
+package nepsAcademy.ola;
+
+/*Esse é o primeiro problema
+
+que você deve resolver. Normalmente problemas terão uma entrada e uma saída esperada, mas por enquanto vamos começar com um problema bem simples, onde você deve apenas imprimir uma mensagem na tela do computador.
+
+Dessa forma, para resolver esse problema você deve apenas imprimir a mensagem "Ola Neps Academy!" na tela do computador.
+
+Para enviar o código contendo sua solução, você deve usar os botões que se encontram no topo dessa página. O botão para enviar sua solução é identificado pelo símbolo do avião de papel.
+
+Nesse problema especial você não precisa ler nenhuma informação como entrada.
+A saída do seu programa deve ser apenas a frase "Ola Neps Academy!" (sem as aspas). Não se esqueça de adicionar o sinal de exclamação no final. */
+
+public class Ola {
+    public static String ola(){
+        return "Ola Neps Academy!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(ola());
+
+    }
+
+}
