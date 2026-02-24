797757e91035281545f4f77065ec66d8f6a9bc36
LuizFilipeA <luizfilipealvessantos@outlook.com>
2026-02-24 13:27:58 -0300

    Neps Academy Exercício 86- Aprovado ou Reprovado

---
+++ JAVA Exercicios/src/nepsAcademy/aprovadoReprovado/AprovadoReprovado.java
@@ -0,0 +1,32 @@
+package nepsAcademy.aprovadoReprovado;
+
+import java.util.Scanner;
+
+public class AprovadoReprovado{
+
+    public static void verificacao(float a, float b){
+        if (a < 0 || a <=10){
+            if (b < 0 || b <=10){
+                float resultado = media(a, b);
+                if (resultado >= 7){
+                    System.out.println("Aprovado");
+                } else if (resultado > 4) {
+                    System.out.println("Recuperacao");
+                }else{
+                    System.out.println("Reprovado");
+                }
+            }
+        }
+    }
+
+    public static float media(float a, float b){
+        return (a+b)/2;
+    }
+
+    public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            float a = sc.nextFloat();
+            float b= sc.nextFloat();
+            verificacao(a, b);
+    }
+}
