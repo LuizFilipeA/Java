8f18f06d005b11fdb33930635270cee218e53f40
LuizFilipeA <luizfilipealvessantos@outlook.com>
2026-04-14 20:30:18 -0300

    Ex Veiculo - Classe Main

---
+++ JAVA Exercicios/src/poo/Heranca/dois/veiculo/Main.java
@@ -0,0 +1,11 @@
+package poo.Heranca.dois.veiculo;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Carro carro = new Carro();
+        carro.mover();
+        carro.buzinar();
+    }
+
+}
