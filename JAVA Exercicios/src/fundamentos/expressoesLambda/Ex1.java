72ae80f5803dfe1afb9315edab53788b25800801
LuizFilipeA <luizfilipealvessantos@outlook.com>
2026-04-27 20:36:31 -0300

    Streams Ex1 - Filter, Map e Collect

---
+++ JAVA Exercicios/src/fundamentos/expressoesLambda/Ex1.java
@@ -0,0 +1,12 @@
+package fundamentos.expressoesLambda;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Ex1 {
+    public static void main(String[] args) {
+
+        List<Integer> numeros = Arrays.asList(1, 8, 3, 10, 5, 15, 20);
+
+    }
+}
