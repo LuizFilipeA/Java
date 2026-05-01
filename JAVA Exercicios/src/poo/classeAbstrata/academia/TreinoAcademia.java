5f6b980edb3098e74fe7a335b9da9d15e79f70f6
LuizFilipeA <luizfilipealvessantos@outlook.com>
2026-05-01 14:19:31 -0300

    Classe abstrata pai - TreinoAcademia

---
+++ JAVA Exercicios/src/poo/classeAbstrata/academia/TreinoAcademia.java
@@ -0,0 +1,20 @@
+package poo.classeAbstrata.academia;
+
+abstract class TreinoAcademia {
+
+    void musculacao(){
+        System.out.println("Treino de musculação (super, classe abstrata)");
+    }
+
+    void luta(){
+        System.out.println("Treino de luta (super, classe abstrata)");
+    }
+
+    void cardio(){
+        System.out.println("Treino de cardio (super, classe abstrata)");
+    }
+
+    void danca(){
+        System.out.println("Treino de dança (super, classe abstrata)" );
+    }
+}
