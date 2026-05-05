c7cc397ecebcb222c461b3bef15e24d9594d5557
LuizFilipeA <luizfilipealvessantos@outlook.com>
2026-05-05 20:18:39 -0300

    Casting - De String para int (parseInt)

---
+++ JAVA Exercicios/src/fundamentos/casting/Casting.java
@@ -0,0 +1,38 @@
+package fundamentos.casting;
+
+public class Casting {
+    public static void main(String[] args) {
+        // Exemplo De int para double (implícito)
+        int valorInteiro = 3;
+        double valorDouble = valorInteiro;
+        System.out.println("valorInteiro: "+valorInteiro+" valorDouble: "+valorDouble); //(int = 3, double = 3.00)
+
+        //Exemplo De double para int
+        float n3 = 3.30f;
+        int n4;
+        n4 = (int)n3; //Um casting explícito, o java entende que nós aceitamos perder as casas decimais
+        System.out.println("n3: "+n3+" n4: "+n4); //(float = 3.30, int =3)
+
+        //Exemplo De char para string
+        char letra= 's';
+        String palavra = String.valueOf(letra);
+        System.out.println("String: "+palavra);
+
+        //Exemplo De string para char
+        String s = "String"; //Uma string nada mais é do que um vetor de caracteres
+        char c = s.charAt(5); //Com a função charAt, somos obrigados a informar o indice, ou seja, qual char da string nós queremos armazenar, neste caso, armazenamos o char de posição 5 da palavra String, ou seja, o char "g"
+        System.out.println("Char: "+c);
+
+        //Exemplo De int para string
+        String teste = String.valueOf(n4);
+        System.out.println("Sim, isso é uma string"+teste);
+
+        //String para int
+        String numeroString = "10";
+        int n5 = Integer.parseInt(numeroString);
+        System.out.println(n5);
+
+        //
+    }
+
+}
