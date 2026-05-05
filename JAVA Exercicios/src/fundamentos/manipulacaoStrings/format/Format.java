4c4fbcb627e894799d04c51dfe7b2e186f23458e
LuizFilipeA <luizfilipealvessantos@outlook.com>
2026-05-05 20:26:30 -0300

    Revisao - String formatada (format)

---
+++ JAVA Exercicios/src/fundamentos/manipulacaoStrings/format/Format.java
@@ -0,0 +1,24 @@
+package fundamentos.manipulacaoStrings.format;
+
+public class Format {
+
+    public static void main(String[] args) {
+        String nome = "Luiz Felipe";
+        int idade = 26;
+
+        //Format é uma função da classe string (String.format()) que pode formatar strings
+
+        //No exemplo abaixo, format dentro de um sout, primeiro parametro é a nossa string formatada e os outros parametros sao as variáveis que iremos utilizar no print
+
+        System.out.println(String.format("Olá, meu nome é %s e tenho %d anos", nome, idade)); //%s e %d indicam string e um decimal
+
+        // Pode ser armazenada dentro de uma variável também
+        String format = String.format("Olá, meu nome é %s e tenho %d anos", nome, idade);
+        System.out.println(format);
+
+        //"Estilo tabuada "
+        int n1 = 1;
+        int n2 = 2;
+        System.out.println(String.format("%d x %d = %d", n1, n2, n1*n2));
+    }
+}
