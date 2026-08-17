b37175cf23c0df2549e84cf88420f5795cc91874
LuizFilipeA <luizfilipealvessantos@outlook.com>
2026-08-17 20:31:35 -0300

    Sistema RH - Classe Abstrata Funcionario

--- JAVA Exercicios/src/poo/rh/modelos/Funcionario.java
+++ JAVA Exercicios/src/poo/rh/modelos/Funcionario.java
@@ -4,12 +4,20 @@ public abstract class Funcionario implements Contratado {
 
     private String nome;
     private String cargo;
-    private double pagamento;
+    private double salarioBase;
 
-    public Funcionario(String nome, String cargo, double pagamento){
-        this.nome = nome;
+    public Funcionario(String nome, String cargo, double salarioBase){
+
+        if (verificaPositivo(salarioBase)) {
+            this.salarioBase = salarioBase;
+        } else {
+            throw new IllegalArgumentException("Salário base deve ser positivo");
+        }if (nome != null && !nome.isEmpty() && cargo != null && !cargo.isEmpty()){
+            this.nome = nome;
+            this.cargo = cargo;
+        }
+
         this.cargo = cargo;
-        this.pagamento = pagamento;
     }
 
     public String getNome() {
@@ -20,11 +28,12 @@ public abstract class Funcionario implements Contratado {
         return cargo;
     }
 
+    public double getPagamento() {
+        return salarioBase;
+    }
+
     @Override
     public boolean verificaPositivo(double valor) {
-        if (valor <= 0){
-            return false;
-        }
-        return true;
+      return !(valor <=0);
     }
 }
