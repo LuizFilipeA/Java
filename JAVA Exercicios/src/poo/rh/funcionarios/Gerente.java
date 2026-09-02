ade309284bd5461145df3026ea92c9ae5fcad82e
LuizFilipeA <luizfilipealvessantos@outlook.com>
2026-09-02 06:48:22 -0300

    Sistema RH - Sub Classe Gerente - Refatoracao

--- JAVA Exercicios/src/poo/rh/funcionarios/Gerente.java
+++ JAVA Exercicios/src/poo/rh/funcionarios/Gerente.java
@@ -5,14 +5,24 @@ import poo.rh.modelos.Funcionario;
 public class Gerente extends Funcionario {
 
     private double bonusGestao;
+    private double salarioBase;
 
     public Gerente(String nome, double salarioBase, double bonusGestao) {
-        super(nome, "Gerente", salarioBase);
-        this.bonusGestao = bonusGestao;
+        super(nome);
+        if (verificaPositivo(salarioBase)){
+            this.salarioBase = salarioBase;
+        }else {
+            throw new IllegalArgumentException("Salário base deve ser positivo");
+        }
+        if (verificaPositivo(bonusGestao)){
+            this.bonusGestao = bonusGestao;
+        }else {
+            throw new IllegalArgumentException("Bonus de gestão deve ser positivo");
+        }
     }
 
     @Override
     public double calculaPagamento() {
-        return (getPagamento() + bonusGestao);
+        return (salarioBase + bonusGestao);
     }
 }
