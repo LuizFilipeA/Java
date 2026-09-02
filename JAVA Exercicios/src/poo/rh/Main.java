454146993a54c130e6c650f0c8b12713bc1db583
LuizFilipeA <luizfilipealvessantos@outlook.com>
2026-09-02 06:50:09 -0300

    Sistema RH - Main v2 - Refatoracao

--- JAVA Exercicios/src/poo/rh/Main.java
+++ JAVA Exercicios/src/poo/rh/Main.java
@@ -1,43 +1,39 @@
 package poo.rh;
 
-import poo.rh.funcionarios.Desenvolvedor;
-import poo.rh.funcionarios.Estagiario;
-import poo.rh.funcionarios.FreeLancer;
-import poo.rh.funcionarios.Gerente;
+import poo.rh.funcionarios.*;
 import poo.rh.modelos.Funcionario;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        List<Funcionario> funcionarios = new ArrayList<>();
         double folhaSalarial = 0;
-        double salarioMedio = 0;
         double menorSalario = 0;
         double maiorSalario = 0;
-        ArrayList<Funcionario> funcionarios = new ArrayList<>();
-        Desenvolvedor desenvolvedor = new Desenvolvedor("João", 5000, 1000);
-        Estagiario estagiario = new Estagiario("Maria", 2000);
-        Gerente gerente = new Gerente("Andressa", 10000, 2000);
-        FreeLancer freeLancer = new FreeLancer("Carlos", 300, 40);
 
-        funcionarios.add(desenvolvedor);
-        funcionarios.add(estagiario);
-        funcionarios.add(gerente);
-        funcionarios.add(freeLancer);
+        funcionarios.add(new Consultor("Carlos", 100, 20));
+        funcionarios.add(new Desenvolvedor("João", 5000, 1000));
+        funcionarios.add(new Estagiario("Maria", 2000));
+        funcionarios.add(new FreeLancer("Pedro", 200, 40));
+        funcionarios.add(new Gerente("Andressa", 10000, 2000));
+        funcionarios.add(new Analista("Lucas", 4000, 500));
 
         for (Funcionario funcionario : funcionarios) {
-            System.out.println("Nome: " + funcionario.getNome() + ", Cargo: " + funcionario.getCargo() + ", Salário: " + funcionario.calculaPagamento());
-               folhaSalarial += funcionario.calculaPagamento();
-            if(funcionario.calculaPagamento() < menorSalario || menorSalario == 0){
-                menorSalario = funcionario.calculaPagamento();
+            double pagamento = funcionario.calculaPagamento();
+            System.out.println("Nome: " + funcionario.getNome() +", Salário: " + pagamento);
+               folhaSalarial += pagamento;
+            if(pagamento < menorSalario || menorSalario == 0){
+                menorSalario = pagamento;
             }
-            if(funcionario.calculaPagamento() > maiorSalario){
-                maiorSalario = funcionario.calculaPagamento();
+            if(pagamento > maiorSalario){
+                maiorSalario = pagamento;
             }
         }
-        salarioMedio = folhaSalarial / funcionarios.size();
+        double salarioMedio = folhaSalarial / funcionarios.size();
 
         System.out.printf("Folha salarial: %.2f\nMedia salarial: %.2f\nMenor salário: %.2f\nMaior salário: %.2f\n", folhaSalarial, salarioMedio, menorSalario, maiorSalario);
     }
