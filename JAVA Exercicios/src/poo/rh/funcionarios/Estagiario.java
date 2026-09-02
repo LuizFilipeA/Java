b571f2c2a3574ddf020a752e84b7df637b9b2ab4
LuizFilipeA <luizfilipealvessantos@outlook.com>
2026-09-02 06:47:42 -0300

    Sistema RH - Sub Classe Estagiario - Refatoracao

--- JAVA Exercicios/src/poo/rh/funcionarios/Estagiario.java
+++ JAVA Exercicios/src/poo/rh/funcionarios/Estagiario.java
@@ -4,12 +4,19 @@ import poo.rh.modelos.Funcionario;
 
 public class Estagiario extends Funcionario{
 
+    private double salarioBase;
+
     public Estagiario(String nome, double salarioBase) {
-        super(nome, "Estagiario", salarioBase);
+        super(nome);
+        if (verificaPositivo(salarioBase)) {
+            this.salarioBase = salarioBase;
+        }else  {
+            throw new IllegalArgumentException("Salario base deve ser positivo");
+        }
     }
 
     @Override
     public double calculaPagamento() {
-        return getPagamento();
+        return salarioBase;
     }
 }
